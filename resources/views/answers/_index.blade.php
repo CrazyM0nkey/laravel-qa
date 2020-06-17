df5561c7280e7e21bf69e8164828acff79065051
CrazyM0nkey <crazym0nkey@protonmail.com>
2020-06-18 01:07:50 +0200

    Implement accept the answer as best answer

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -7,7 +7,6 @@
                 </div>
                 <hr>
                 @include('layouts._messages')
-
                 @foreach ($answers as $answer)
                 <div class="media">
                     <div class="d-flex flex-column vote-controls">
@@ -18,10 +17,22 @@
                         <a title="This answer is not useful" class="vote-down off">
                             <i class="fas fa-caret-down fa-3x"></i>
                         </a>
-                        <a title="Mark this answer as best answer" class="{{ $answer->status }} mt-2">
+                        @can('accept', $answer)
+                        <a title="Mark this answer as best answer" class="{{ $answer->status }} mt-2"
+                            onclick="event.preventDefault(); document.getElementById('accept-answer-{{ $answer->id }}').submit();">
                             <i class="fas fa-check fa-2x"></i>
                         </a>
-                        <span class="favorites-count">123</span>
+                        <form action="{{ route('answers.accept', $answer->id) }}" id="accept-answer-{{ $answer->id }}"
+                            method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                        @else
+                            @if ($answer->is_best)
+                                <a title="The question owner marked this quetstion as best question" class="{{ $answer->status }} mt-2">
+                                    <i class="fas fa-check fa-2x"></i>
+                                </a>
+                            @endif
+                        @endcan
                     </div>
                     <div class="media-body">
                         {!! $answer->body !!}
--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,4 @@ Route::resource('questions', 'QuestionsController')->except('show');
 //Route::post('/questions/{question}/answers', 'AnswersController@store')->name('answers.store');
 Route::resource('questions.answers', 'AnswersController')->except(['index', 'show', 'create']);   //only(['store', 'edit', 'update', 'destroy']);
 Route::get('/questions/{slug}', 'QuestionsController@show')->name('questions.show');
+Route::post('/answers/{answer}/accept', 'AcceptAnswerController')->name('answers.accept');
