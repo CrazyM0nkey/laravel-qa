fd4949625b047bb88bcb715865f94351b0c9479b
CrazyM0nkey <crazym0nkey@protonmail.com>
2020-06-17 05:24:18 +0200

    Implement update answer

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -7,7 +7,7 @@
                 </div>
                 <hr>
                 @include('layouts._messages')
-                
+
                 @foreach ($answers as $answer)
                 <div class="media">
                     <div class="d-flex flex-column vote-controls">
@@ -25,21 +25,45 @@
                     </div>
                     <div class="media-body">
                         {!! $answer->body !!}
-                        <div class="float-right mt-3">
-                            <span class="text-muted mb-1">
-                                Answered {{ $answer->created_date }}
-                            </span>
-                            <div class="media mt-1">
-                                <a href="{{ $answer->user->url }}" class="pr-2">
-                                    <img src="{{ $answer->user->avatar }}" alt="" srcset="">
-                                </a>
-                                <div class="media-body mt-2">
-                                    <a href="{{ $answer->user->url }}">
-                                        {{ $answer->user->name }}
+                        <div class="row">
+                            <div class="col-4">
+                                <div class="ml-auto">
+                                    @can('update', $answer)
+                                    <a href="{{ route('questions.answers.edit',[ $question->id, $answer->id ]) }}"
+                                        class="btn btn-sm btn-outline-info">Edit</a>
+                                    @endcan
+                                    @can('delete', $answer)
+                                    <form class="form-delete" method="post" style="min-width: 150px"
+                                        action="{{ route('questions.answers.destroy', [ $question->id, $answer->id ]) }}">
+                                        @method('DELETE')
+                                        @csrf
+                                        <button type="submit" class="btn btn-sm btn-outline-danger"
+                                            onclick="return confirm('Are you sure?')">Delete</button>
+                                    </form>
+                                    @endcan
+
+                                </div>
+                            </div>
+                            <div class="col-4">
+
+                            </div>
+                            <div class="col-4">
+                                <span class="text-muted mb-1">
+                                    Answered {{ $answer->created_date }}
+                                </span>
+                                <div class="media mt-1">
+                                    <a href="{{ $answer->user->url }}" class="pr-2">
+                                        <img src="{{ $answer->user->avatar }}" alt="" srcset="">
                                     </a>
+                                    <div class="media-body mt-2">
+                                        <a href="{{ $answer->user->url }}">
+                                            {{ $answer->user->name }}
+                                        </a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
+
                     </div>
                 </div>
                 <hr>
