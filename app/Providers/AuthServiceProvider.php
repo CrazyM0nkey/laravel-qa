c6ca5cd73ccab8b75c9273e86ab7c21178394db4
CrazyM0nkey <crazym0nkey@protonmail.com>
2020-06-17 12:13:59 +0200

    Implement delete functionality for answer

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -7,7 +7,6 @@ use App\Policies\AnswerPolicy;
 use App\Policies\QuestionPolicy;
 use App\Question;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
