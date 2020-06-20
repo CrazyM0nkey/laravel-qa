0bddf43fbe00fd296810b5762cf0512917af7a59
CrazyM0nkey <crazym0nkey@protonmail.com>
2020-06-20 04:36:23 +0200

    Implement DRY principle on the appplication views

---
+++ resources/views/shared/_author.blade.php
@@ -0,0 +1,13 @@
+<span class="text-muted mb-1">
+    {{ $label . " " . $model->created_date }}
+</span>
+<div class="media mt-1">
+    <a href="{{ $model->user->url }}" class="pr-2">
+        <img src="{{ $model->user->avatar }}" alt="" srcset="">
+    </a>
+    <div class="media-body mt-2">
+        <a href="{{ $model->user->url }}">
+            {{ $model->user->name }}
+        </a>
+    </div>
+</div>
